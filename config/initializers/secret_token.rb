ae771a54ad0bf78230395104e1eaca26a4f26d1d
lucci201 <lucci201@student.liu.se>
2013-11-08 13:34:25 +0100

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FindService::Application.config.secret_key_base = 'a325744c80b9c8b6307c97a50f07121dc77e1fe174629a19b31b743292d4226a494d3b1f67155feb0e44096961df3ad54e64a32f386342132b98ab01dac3871f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FindService::Application.config.session_store :cookie_store, key: '_FindService_session'
